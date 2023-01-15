ac847da662d453cc18c4f9a9ba6a149d07de0f3a
Eliaser Concha <eliaserdev99@gmail.com>
2023-01-15 14:16:43 -0300

    fix(frontend)!: Responsive 400px
    Diseño de las vistas login y home adaptado a una resolución de pantalla de 400 px como máximo.

--- frontend/src/components/HorizontalNavBar.jsx
+++ frontend/src/components/HorizontalNavBar.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+import Swal from "sweetalert2"
 import useAuthStore from "../hooks/useAuthStore"
 import usePeriod from "../hooks/usePeriod"
 
@@ -5,6 +7,28 @@ const HorizontalNavBar = () => {
 
     const {user, startLogOut} = useAuthStore()
     const {period, periodValue, updatePeriodState, updatePeriodTranslateState} = usePeriod(user.id)
+
+    const [responsiveDropdown, setResponsiveDropdown] = useState(false)
+
+    const handleResponsiveDropdown = (e) => {
+        setResponsiveDropdown(!responsiveDropdown)
+        var dropdown = document.getElementById("dropdown-access")
+        var icon_select = document.getElementById("icon-responsive")
+        var select = document.getElementById("option_select_period")
+        if (responsiveDropdown) {
+            dropdown.style.display = "flex"
+            icon_select.classList.remove("text-white")
+            icon_select.classList.add("text-black")
+            select.classList.remove("text-white")
+            select.classList.add("text-black")
+        } else {
+            dropdown.style.display = "none"
+            icon_select.classList.remove("text-black")
+            icon_select.classList.remove("text-white")
+            select.classList.remove("text-black")
+            select.classList.add("text-white")
+        }
+    }
 
     const handleSelectPeriod = (e) => {
         var val = e.target.value
@@ -15,14 +39,33 @@ const HorizontalNavBar = () => {
         }
     }
 
+    const handleLogOut = () => {
+        Swal.fire({
+            title: "¿Desea finalizar la sesión?",
+            icon: "question",
+            showConfirmButton: true,
+            confirmButtonText: "Sí",
+            showDenyButton: true,
+            denyButtonText: "No"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                startLogOut()
+            }
+        })
+    }
+
     return <div className="navbar-h">
         <h3 className="title">Harvest Manager</h3>
-        <ul className="access">
+        <a className="dropdown-plus" onClick={handleResponsiveDropdown}>
+            <input type="checkbox" name="plus" className="plus-dropdown" />
+            <label htmlFor="plus"><i className="bi bi-plus-circle-fill"></i></label>
+        </a>
+        <ul id="dropdown-access" className="access">
             <li>{"Bienvenido(a) " + user.name}</li>
             <li>
                 <div className="form-group">
-                    <i className="input-icon bi bi-calendar text-white"></i>
-                    <select onInput={handleSelectPeriod} className="form-style text-white">
+                    <i id="icon-responsive" className="input-icon bi bi-calendar text-white"></i>
+                    <select id="option_select_period" onInput={handleSelectPeriod} className="form-style text-white">
                         <option value="default" className="text-black">Selecciona un mes</option>
                         {
                             period.map((value, key) => <option className="text-black" key={key} value={periodValue[key]}>{value}</option>)
@@ -30,7 +73,7 @@ const HorizontalNavBar = () => {
                     </select>
                 </div>
             </li>
-            <li className="btn no-width-100" onClick={startLogOut}>Cerrar Sesión</li>
+            <li className="btn no-width-100" onClick={handleLogOut}>Cerrar Sesión</li>
         </ul>
     </div>
 }
--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,42 +1,45 @@
-import { useEffect } from "react"
-import Swal from "sweetalert2"
-import {Checkbox} from "../components/Checkbox"
-import {LogInForm} from "../components/LogInForm"
-import {SignUpForm} from "../components/SignUpForm"
-import useAuthStore from "../hooks/useAuthStore"
+import { useEffect } from "react";
+import Swal from "sweetalert2";
+import { Checkbox } from "../components/Checkbox";
+import { LogInForm } from "../components/LogInForm";
+import { SignUpForm } from "../components/SignUpForm";
+import useAuthStore from "../hooks/useAuthStore";
 
 export const LogIn = () => {
-
-  const {errorMessage} = useAuthStore()
+  const { errorMessage } = useAuthStore();
 
   useEffect(() => {
     if (errorMessage !== undefined) {
       Swal.fire({
         title: "Error en la autenticación",
         icon: "error",
-        text: errorMessage
-      })
+        text: errorMessage,
+      });
     }
-  }, [errorMessage])
+  }, [errorMessage]);
 
-  return (<div className="register-container">
-    <div className="col align-self-center">
-      <div className="section">
-        <Checkbox />
-        <div className="card-3d-wrap">
-          <div className="card-3d-wrapper">
-            <div className="card-front">
-              <div className="center-wrap">
-               <div className="section">
-                <LogInForm />
-               </div>
-              </div>
-            </div>
+  return (
+    <div className="register-container">
+      <div className="row">
+        <div className="col align-self-center">
+          <div className="section">
+            <Checkbox />
+            <div className="card-3d-wrap">
+              <div className="card-3d-wrapper">
+                <div className="card-front">
+                  <div className="center-wrap">
+                    <div className="section">
+                      <LogInForm />
+                    </div>
+                  </div>
+                </div>
 
-            <div className="card-back">
-              <div className="center-wrap">
-                <div className="section">
-                  <SignUpForm /> 
+                <div className="card-back">
+                  <div className="center-wrap">
+                    <div className="section">
+                      <SignUpForm />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -44,5 +47,5 @@ export const LogIn = () => {
         </div>
       </div>
     </div>
-  </div>)
-}
+  );
+};
