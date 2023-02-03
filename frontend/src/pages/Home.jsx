147a006ded095c11439c958a2c69ae4c41643a58
Eliaser Concha <eliaserdev99@gmail.com>
2023-02-02 23:20:44 -0300

    test4

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -92,6 +92,7 @@ export const Home = () => {
       const res = await fetch(import.meta.env.VITE_API_URL+"/api/blueberry/pdf", {
         method: "POST",
         headers: {
+          "Content-Type": "application/pdf",
           "Access-Control-Allow-Origin": "https://harvestmanager.onrender.com",
           "Access-Control-Allow-Methods": "POST"
         },
