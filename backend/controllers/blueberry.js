d382b642a274beba6be1880d8f394116dfd2ae2a
Eliaser Concha <eliaserdev99@gmail.com>
2023-02-03 00:16:38 -0300

    test 9

--- backend/controllers/blueberry.js
+++ backend/controllers/blueberry.js
@@ -95,10 +95,9 @@ const GetMonths = async (req = request, res = response) => {
 }
 
 const GeneratePDF = async (req = request, res = response) => {
-
     try {
         console.log(req.headers)
-        console.log(req.files)
+        console.log(req.body)
         const image = req.files.file.tempFilePath
         
         const user_id = req.body["user_id"]
